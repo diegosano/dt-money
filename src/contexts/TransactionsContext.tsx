3d8ffc80b07a56232e29cb09af0b881b9e4dbf87
Diego Sano <diegocsano@outlook.com>
2023-05-04 01:44:23 -0300

    feat: create transactions provider context and hook

---
+++ src/contexts/TransactionsContext.tsx
@@ -0,0 +1,41 @@
+import { createContext, useEffect, useState } from 'react';
+import type { ReactNode } from 'react';
+
+interface Transaction {
+  id: number;
+  description: string;
+  type: 'income' | 'outcome';
+  price: number;
+  category: string;
+  createdAt: string;
+}
+
+interface TransactionsContextType {
+  transactions: Transaction[];
+}
+
+export const TransactionsContext = createContext({} as TransactionsContextType);
+
+interface TransactionsProviderProps {
+  children: ReactNode;
+}
+
+export function TransactionsProvider({ children }: TransactionsProviderProps) {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3333/api/transactions')
+      .then((response) => response.json())
+      .then((data) => setTransactions(data));
+  }, []);
+
+  return (
+    <TransactionsContext.Provider
+      value={{
+        transactions,
+      }}
+    >
+      {children}
+    </TransactionsContext.Provider>
+  );
+}
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,27 +1,12 @@
-import { useEffect, useState } from 'react';
 import { Header } from '../../components/Header';
 import { SearchForm } from '../../components/SearchForm';
 import { Summary } from '../../components/Summary';
-
 import * as S from './styles';
 
-interface Transaction {
-  id: number;
-  description: string;
-  type: 'income' | 'outcome';
-  price: number;
-  category: string;
-  createdAt: string;
-}
+import { useTransactions } from '../../hooks/useTransactions';
 
 export function Transactions() {
-  const [transactions, setTransactions] = useState<Transaction[]>([]);
-
-  useEffect(() => {
-    fetch('http://localhost:3333/api/transactions')
-      .then(response => response.json())
-      .then(data => setTransactions(data))
-  }, [])
+  const { transactions } = useTransactions()
 
   return (
     <div>
