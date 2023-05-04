d495f23d1664c4fd3586479616d16dab05f8c666
Diego Sano <diegocsano@outlook.com>
2023-05-04 01:57:07 -0300

    feat: create date and number formatters

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,7 +1,9 @@
 import { ArrowCircleUp, ArrowCircleDown, CurrencyDollar } from 'phosphor-react';
 
 import * as S from './styles';
+
 import { useTransactions } from '../../hooks/useTransactions';
+import { priceFormatter } from '../../utils/formatter';
 
 export function Summary() {
   const { transactions } = useTransactions();
@@ -33,7 +35,7 @@ export function Summary() {
           <ArrowCircleUp size={32} color="#00b37e" />
         </header>
 
-        <strong>{summary.income}</strong>
+        <strong>{priceFormatter.format(summary.income)}</strong>
       </S.SummaryCard>
 
       <S.SummaryCard>
@@ -42,7 +44,7 @@ export function Summary() {
           <ArrowCircleDown size={32} color="#f75a68" />
         </header>
 
-        <strong>{summary.outcome}</strong>
+        <strong>{priceFormatter.format(summary.outcome)}</strong>
       </S.SummaryCard>
 
       <S.SummaryCard variant="green">
@@ -51,7 +53,7 @@ export function Summary() {
           <CurrencyDollar size={32} color="#fff" />
         </header>
 
-        <strong>{summary.total}</strong>
+        <strong>{priceFormatter.format(summary.total)}</strong>
       </S.SummaryCard>
     </S.SummaryContainer>
   );
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -4,9 +4,10 @@ import { Summary } from '../../components/Summary';
 import * as S from './styles';
 
 import { useTransactions } from '../../hooks/useTransactions';
+import { dateFormatter, priceFormatter } from '../../utils/formatter';
 
 export function Transactions() {
-  const { transactions } = useTransactions()
+  const { transactions } = useTransactions();
 
   return (
     <div>
@@ -18,17 +19,18 @@ export function Transactions() {
 
         <S.TransactionsTable>
           <tbody>
-            {transactions.map(transaction => (
-            <tr key={transaction.id}>
-              <td width="50%">{transaction.description}</td>
-              <td>
-                <S.PriceHighlight variant={transaction.type}>
-                  {transaction.price}
-                </S.PriceHighlight>
-              </td>
-              <td>{transaction.category}</td>
-              <td>{transaction.createdAt}</td>
-            </tr>
+            {transactions.map((transaction) => (
+              <tr key={transaction.id}>
+                <td width="50%">{transaction.description}</td>
+                <td>
+                  <S.PriceHighlight variant={transaction.type}>
+                    {transaction.type === 'outcome' && '- '}
+                    {priceFormatter.format(transaction.price)}
+                  </S.PriceHighlight>
+                </td>
+                <td>{transaction.category}</td>
+                <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
+              </tr>
             ))}
           </tbody>
         </S.TransactionsTable>
